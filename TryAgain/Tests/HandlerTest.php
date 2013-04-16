44b91cd13072037e8d16683adca2e52854c0912d
villfa <fabien.villepinte@gmail.com>
2013-04-16 23:42:59 +0200

    improve test coverage

--- TryAgain/Tests/HandlerTest.php
+++ TryAgain/Tests/HandlerTest.php
@@ -3,24 +3,61 @@
 namespace TryAgain\Tests;
 
 use TryAgain\Handler;
+use \Mockery as m;
 
 class HandlerTest extends \PHPUnit_Framework_TestCase
 {
-    private function getHandler()
-    {
-        return new Handler();
-    }
-
     public function testSimpliestCase()
     {
         $add = function ($a, $b) {
             return $a + $b;
         };
 
-        $h = $this->getHandler();
+        $h = new Handler;
+        $this->assertEquals(0, $h->getNbTries());
         $this->assertEquals(5, $h->execute($add, array(2, 3)));
         $this->assertEquals(5, $h->getLastResult());
         $this->assertEquals(1, $h->getNbTries());
         $this->assertNull($h->getLastException());
+    }
+    
+    public function testThrowExceptionAfterFewTries()
+    {
+        $func = function () {
+            throw new \RuntimeException();
+        };
+    
+        $validator = m::mock('TryAgain\ValidatorInterface');
+        $validator->shouldReceive('mustRetry')->andReturnUsing(
+            function ($handler) {
+                return $handler->getNbTries() < 3;
+            }
+        );
+        
+        $h = new Handler($validator);
+        
+        try {
+            $h->execute($func);
+        } catch (\Exception $exception) {
+            $this->assertInstanceOf('\RuntimeException', $exception);
+            $this->assertNull($h->getLastResult());
+            $this->assertEquals(3, $h->getNbTries());
+            return;
+        }
+        
+        $this->fail('No exception thrown');
+    }
+    
+    public function testIntervalIsCalled()
+    {
+        $validator = m::mock('TryAgain\ValidatorInterface');
+        $validator->shouldReceive('mustRetry')->andReturn(true, true, false);
+    
+        $interval = m::mock('TryAgain\IntervalInterface');
+        $interval->shouldReceive('process')->times(2); // process() must be called twice
+    
+        $h = new Handler($validator, $interval);
+        $h->execute(function () {});
+        $this->assertEquals(3, $h->getNbTries());
     }
 }
