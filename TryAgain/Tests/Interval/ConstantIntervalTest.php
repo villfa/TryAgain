8b8a34b61cde2a1594c2c3947afdb4718c502766
villfa <fabien.villepinte@gmail.com>
2013-04-17 22:15:27 +0200

    improve code coverage

--- TryAgain/Tests/Interval/ConstantIntervalTest.php
+++ TryAgain/Tests/Interval/ConstantIntervalTest.php
@@ -3,6 +3,7 @@
 namespace TryAgain\Tests\Interval;
 
 use TryAgain\Interval\ConstantInterval;
+use \Mockery as m;
 
 class ConstantIntervalTest extends \PHPUnit_Framework_TestCase
 {
@@ -12,5 +13,19 @@ class ConstantIntervalTest extends \PHPUnit_Framework_TestCase
             'TryAgain\IntervalInterface',
             new ConstantInterval()
         );
+    }
+    
+    public function testErrorWithWrongDelay()
+    {
+        $this->setExpectedException('\InvalidArgumentException');
+        new ConstantInterval('not a valid delay');
+    }
+    
+    public function testValidCase()
+    {
+        $interval = new ConstantInterval(0.2);
+        $startTime = microtime(true);
+        $interval->process(m::mock('TryAgain\Handler'));
+        $this->assertGreaterThanOrEqual(0.2, microtime(true) - $startTime);
     }
 }
